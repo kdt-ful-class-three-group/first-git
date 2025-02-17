45935c3b98bbe02d10fce1d8d4102469b46b640d
김민지 <qniomo00@gmail.com>
2025-02-17 16:52:21 +0900

    <div>와 <img>를 만드는 위치를 잘못 넣음

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let btn = document.getElementsByTagName("button")[0];
 
+let div = document.createElement("div");
+document.body.append(div);
+let img = document.createElement("img");
+div.append(img);
+
 btn.addEventListener("click", () => {
   let input = document.getElementsByTagName("input");
 
   console.log(input[0].checked, input[1].checked);
 
-  let div = document.createElement("div");
-  document.body.append(div);
-  let img = document.createElement("img");
-  div.append(img);
   if (input[0].checked === true && input[1].checked === false) {
     img.src = "./img/yes.jpg";
     img.alt = "yes";
