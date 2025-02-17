bf00c696888ce0067486fd083f2c33a1d30aea5b
김민지 <qniomo00@gmail.com>
2025-02-17 17:00:27 +0900

    수정

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let btn = document.getElementsByTagName("button")[0];
 
 let div = document.createElement("div");
 document.body.append(div);
+
 let img = document.createElement("img");
 div.append(img);
+div.style.marginTop = "20px";
 
 btn.addEventListener("click", () => {
   let input = document.getElementsByTagName("input");
