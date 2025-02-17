3317792cacd51cef60bb928662bf84ecd4f23799
김민지 <qniomo00@gmail.com>
2025-02-17 15:33:34 +0900

    button click했을 때 img

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let btn = document.getElementsByTagName("button")[0];
 btn.addEventListener("click", () => {
   let input = document.getElementsByTagName("input");
 
-  let div = document.createElement("div");
-  document.body.append(div);
+  if (input[0].value === true && input[1].value === false) {
+    let div = document.createElement("div");
+    document.body.append(div);
+    let img = document.createElement("img");
+    img.src = "./img/yes.jpg";
+    img.alt = "yes";
+
+    div.append(img);
+  }
 });
