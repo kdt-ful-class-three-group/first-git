127f5083698fa2bd8523582b6bfff223c782c60e
김민지 <qniomo00@gmail.com>
2025-02-17 15:30:15 +0900

    button click하면 div 생성

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ let btn = document.getElementsByTagName("button")[0];
 
 btn.addEventListener("click", () => {
   let input = document.getElementsByTagName("input");
+
+  let div = document.createElement("div");
+  document.body.append(div);
 });
