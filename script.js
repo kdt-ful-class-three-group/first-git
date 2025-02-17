79cad2f14b4a5a1d733ab9ff0038e31fcc51bda3
김민지 <qniomo00@gmail.com>
2025-02-17 16:48:09 +0900

    yes 선택하면 yes.jpg 생김

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ let btn = document.getElementsByTagName("button")[0];
 btn.addEventListener("click", () => {
   let input = document.getElementsByTagName("input");
 
-  if (input[0].value === true && input[1].value === false) {
+  console.log(input[0].checked, input[1].checked);
+
+  if (input[0].checked === true && input[1].checked === false) {
     let div = document.createElement("div");
     document.body.append(div);
     let img = document.createElement("img");
     img.src = "./img/yes.jpg";
     img.alt = "yes";
+    // img.width = "100px";
 
     div.append(img);
   }
